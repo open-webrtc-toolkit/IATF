43840f8d7e6b5e8441c7bce5453fe8592d2733e4
Jianjun Zhu <jianjun.zhu@intel.com>
2018-11-05 23:31:19 +0800

    Accept command line arguments for cert and key.

--- src/server/server.js
+++ src/server/server.js
@@ -3,14 +3,15 @@ const fs = require('fs');
 const path = require('path');
 const commander = require('commander');
 
-commander.option('--key_file', 'Key for Socket.IO server.').option(
-  '--certificate_file', 'Certificate for Socket.IO server.').parse(process.argv);
+// `commander`'s properties use undersource-case because arguments are usersource-cased.
+commander.description('IATF server.').option('--key_file <key_file>',
+  'Key for Socket.IO server.').option(
+  '--certificate_file <certificate_file>',
+  'Certificate for Socket.IO server.').parse(process.argv);
 
 const httpsOptions = {
-  key: fs.readFileSync(path.resolve(__dirname, 'certs', 'privatekey.key'),
-    'utf8'),
-  cert: fs.readFileSync(path.resolve(__dirname, 'certs', 'certificate.crt'),
-    'utf8')
+  key: fs.readFileSync(commander.key_file).toString(),
+  cert: fs.readFileSync(commander.certificate_file).toString()
 };
 
 const socketio = require('socket.io').listen(require('https').createServer(
