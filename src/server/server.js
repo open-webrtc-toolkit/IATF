a5fdd3c59f2abb6854d1224789f701c4f3c215d4
Jianjun Zhu <jianjun.zhu@intel.com>
2018-11-13 23:40:22 +0800

    Add a HTTP server for dashboard and maintain test info at server side.

--- src/server/server.js
+++ src/server/server.js
@@ -1,7 +1,13 @@
+// Starts a HTTP server for dashboard and a Socket.IO server for testing.
+
 const app = require('express');
 const fs = require('fs');
+const http2 = require('http2');
 const path = require('path');
 const commander = require('commander');
+
+// Map{testId, {roles: map{role, {socket: socket, type: string}}}}.
+const tests = new Map();
 
 // `commander`'s properties use undersource-case because arguments are usersource-cased.
 commander.description('IATF server.').option('--key_file <key_file>',
@@ -14,9 +20,51 @@ const httpsOptions = {
   cert: fs.readFileSync(commander.certificate_file).toString()
 };
 
+const httpServer = http2.createSecureServer(httpsOptions);
+httpServer.listen(8080);
+
 const io = require('socket.io').listen(require('https').createServer(
   httpsOptions, require('express')()).listen(9000));
 
-io.on('connection', socket=>{
-  console.log('A new user connected.');
+// Initialize a test.
+const initTest = function(id, roles) {
+  if (tests.has(id)) {
+    console.warn('Test ID exists.');
+    return false;
+  }
+  const info = {
+    roles: new Map()
+  };
+  for (const role of roles) {
+    info.roles.set(role, {});
+  }
+  tests.set(id, info);
+  return true;
+}
+
+// Emit to message to all other clients in current test.
+const notifyOtherRoles = function(currentRole, roleInfoList, message) {
+
+}
+
+io.on('connection', socket => {
+  // `testId` is a unique string indicates a round of testing.
+  const testId = socket.handshake.query.testId;
+  // Current client's role in this testing.
+  const role = socket.handshake.query.role;
+  // Client type. e.g. JavaScript, iOS, Android.
+  const type = socket.handshake.query.type;
+
+  if (!tests.get(testId)) {
+    console.warn('Invalid test ID.');
+    socket.disconnect(true);
+  }
+  if (!tests.get(testId).roles.has(role)) {
+    console.warn('Invalid role.');
+    socket.disconnect(true);
+  }
+  tests.get(testId).roles.get(role).socket = socket;
+
+  console.log('A new client connected. Test ID: ' + testId + ', role: ' +
+    role + ', type: ' + type);
 });
