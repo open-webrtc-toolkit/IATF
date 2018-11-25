91fb1c2c13e77ba237684f42df710d85edebd82d
Jianjun Zhu <jianjun.zhu@intel.com>
2018-11-25 09:15:57 +0800

    Server accepts REST requests.

--- src/server/server.js
+++ src/server/server.js
@@ -1,13 +1,18 @@
 // Starts a HTTP server for dashboard and a Socket.IO server for testing.
 
-const app = require('express');
+const express = require('express');
 const fs = require('fs');
-const http2 = require('http2');
+const spdy = require('spdy');
 const path = require('path');
 const commander = require('commander');
+const uuid = require('uuid/v4');
 
-// Map{testId, {roles: map{role, {socket: socket, type: string}}}}.
-const tests = new Map();
+const webapp = express();
+webapp.use(express.json()); // to support JSON-encoded bodies
+webapp.use(express.urlencoded()); // to support URL-encoded bodies
+
+// Map{taskId, {roles: map{roleName, {name: string, socket: socket, type: string}}}}.
+const tasks = new Map();
 
 // `commander`'s properties use undersource-case because arguments are usersource-cased.
 commander.description('IATF server.').option('--key_file <key_file>',
@@ -20,51 +25,133 @@ const httpsOptions = {
   cert: fs.readFileSync(commander.certificate_file).toString()
 };
 
-const httpServer = http2.createSecureServer(httpsOptions);
+const httpServer = spdy.createServer(httpsOptions, webapp);
 httpServer.listen(8080);
 
-const io = require('socket.io').listen(require('https').createServer(
-  httpsOptions, require('express')()).listen(9000));
+const io = require('socket.io').listen(httpServer);
 
 // Initialize a test.
-const initTest = function(id, roles) {
-  if (tests.has(id)) {
-    console.warn('Test ID exists.');
-    return false;
+const addTask = function(roles) {
+  let id = uuid();
+  while (tasks.has(id)) {
+    id = uuid();
   }
-  const info = {
+  const task = {
     roles: new Map()
   };
   for (const role of roles) {
-    info.roles.set(role, {});
+    task.roles.set(role.name, {
+      name: role.name,
+      type: role.type
+    });
   }
-  tests.set(id, info);
+  tasks.set(id, task);
+  return id;
+}
+
+// Emit a message to all other clients in current test. Message type is 'iatf-workflow'.
+const sendUserMessage = function(currentRoleName, roleInfoList, message) {
+  for (const role of roleInfoList) {
+    if (role.name !== currentRoleName) {
+      role.socket.emit(message);
+    }
+  }
+}
+
+// Emit a message to all clients in current test. Message type is 'iatf-control'.
+const sendSystemMessage = function(test, message) {
+  for (const role of test.roles.values()) {
+    role.socket.emit(message);
+  }
+}
+
+const areAllRolesReady = function(test) {
+  for (const role of test.roles.values()) {
+    if (!role.socket) {
+      return false;
+    }
+  }
   return true;
 }
 
-// Emit to message to all other clients in current test.
-const notifyOtherRoles = function(currentRole, roleInfoList, message) {
-
-}
-
 io.on('connection', socket => {
-  // `testId` is a unique string indicates a round of testing.
-  const testId = socket.handshake.query.testId;
+  // `taskId` is a unique string indicates a round of testing.
+  const taskId = socket.handshake.query.taskId;
   // Current client's role in this testing.
   const role = socket.handshake.query.role;
   // Client type. e.g. JavaScript, iOS, Android.
   const type = socket.handshake.query.type;
 
-  if (!tests.get(testId)) {
+  if (!tasks.get(taskId)) {
     console.warn('Invalid test ID.');
     socket.disconnect(true);
   }
-  if (!tests.get(testId).roles.has(role)) {
+  if (!tasks.get(taskId).roles.has(role)) {
     console.warn('Invalid role.');
     socket.disconnect(true);
   }
-  tests.get(testId).roles.get(role).socket = socket;
+  const test = tasks.get(taskId);
+  const roleInfo = tasks.get(role);
+  roleInfo.socket = socket;
+  roleInfo.type = type;
 
-  console.log('A new client connected. Test ID: ' + testId + ', role: ' +
+  console.log('A new client connected. Test ID: ' + taskId + ', role: ' +
     role + ', type: ' + type);
+
+  if (areAllRolesReady(tasks.get(taskId))) {
+    sendSystemMessage(test, 'start');
+    // Record start time.
+  }
+});
+
+// Create a new test.
+webapp.put('/rest/tasks', (req, res) => {
+  // req.body is expected to be [{name: string for role name, type: string for device type}].
+  console.log(req);
+  const taskId = addTask(req.body.roles);
+  if (taskId) {
+    return res.send(200, taskId);
+  }
+});
+
+const rolesMapToResponse = function(roles) {
+  const response = {
+    roles: []
+  };
+  for (const [roleId, roleInfo] of roles.roles) {
+    response.roles.push({
+      name: roleInfo.name,
+      type: roleInfo.type
+    });
+  }
+  return response;
+}
+
+const tasksMapToResponse = function() {
+  const response = [];
+  for (const [id, roles] of tasks) {
+    response.push({
+      id: id,
+      roles: rolesMapToResponse(roles)
+    });
+  }
+  return response;
+}
+
+webapp.get('/rest/tasks', (req, res) => {
+  return res.send(tasksMapToResponse())
+});
+
+webapp.get('/rest/tasks/:task', (req, res) => {
+  const id = req.params.task;
+  if (!tasks.has(id)) {
+    res.status(404);
+  }
+  return res.send(rolesMapToResponse(tasks.get(id)));
+})
+
+webapp.use('/web', express.static('LockServerHtmlClient'));
+
+webapp.get('*', (req, res) => {
+  res.send(404, 'Not found');
 });
